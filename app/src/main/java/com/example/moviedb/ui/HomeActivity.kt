cb5f43bb0f06b279e0cf83064f946e963b918643
CIANDT\dsaleth <P0654679>
2023-05-19 10:50:08 -0300

    Start do projeto

---
+++ app/src/main/java/com/example/moviedb/ui/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.moviedb.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moviedb.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
